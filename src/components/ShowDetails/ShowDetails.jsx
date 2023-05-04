a040556959a4257ce638368378923851e2bfece3
ErastKoliasa <erastyk28@gmail.com>
2023-05-04 22:29:58 +0300

    added styles for show details component

--- src/components/ShowDetails/ShowDetails.jsx
+++ src/components/ShowDetails/ShowDetails.jsx
@@ -1,33 +1,36 @@
 import photo from '../../img/missing.jpg'
+import style from './ShowDetails.module.css'
 const ShowDetails = ({ showDetails }) => {
 
-    const removeTags = (text) => {
-        if (text === null || text === "") {
-          return false;
-        } else {
-          text = text.toString();
-        }
-        return text.replace(/(<([^>]+)>)/gi, "");
-      };
+  const removeTags = (text) => {
+    if (text === null || text === "") {
+      return false;
+    } else {
+      text = text.toString();
+    }
+    return text.replace(/(<([^>]+)>)/gi, "");
+  };
 
-    return (
-        <div>
-            {showDetails.image ? <img src={showDetails.image.medium} alt="Movie banner" /> : <img src={photo} alt="Movie banner is missing" />}
-            <h2>{showDetails.name}</h2>
-            GENRE:
-            <ul>
-                {showDetails.genres.map((genre, index) => (
-                    <li key={index}>{genre}</li>))}
-            </ul>
-            <p>RATING: {showDetails.rating.average ? showDetails.rating.average : "No rating"}</p>
-            <p>LINK TO SHOW: {showDetails.officialSite ? <a href={showDetails.officialSite} target='blank'>{showDetails.officialSite}</a> : "No oficcial site"}</p>
-            <p>STATUS: {showDetails.status ? showDetails.status : "No status"}</p>
-            <p>SCHEDULE: {showDetails.schedule.time ? showDetails.schedule.time : "No time"} -
-                {showDetails.schedule.days === []? showDetails.schedule.days.map((day, index) => <span key={index}>{day}</span>) : " No day"}</p>
-            <p>SUMMARY: {showDetails.summary ? removeTags(showDetails.summary) : "No summary"}</p>
+  return (
+    <div className={style.detailsContainer}>
+      <div>
+        {showDetails.image ? <img src={showDetails.image.medium} alt="Movie banner" /> : <img src={photo} alt="Movie banner is missing" />}
+      </div>
+      <div>
+        <h2>{showDetails.name}</h2>
+        <span className={style.title}>GENRE:</span>
+        {showDetails.genres.map((genre, index) => (
+          <span key={index} className={style.genre}> {genre}</span>))}
+        <p><span className={style.title}>RATING:</span> <span className={style.rating}>{showDetails.rating.average ? showDetails.rating.average : "No rating"}</span></p>
+        <p><span className={style.title}>LINK TO SHOW:</span> {showDetails.officialSite ? <a href={showDetails.officialSite} target='blank'>{showDetails.officialSite}</a> : "No oficcial site"}</p>
+        <p><span className={style.title}>STATUS:</span> {showDetails.status ? showDetails.status : "No status"}</p>
+        <p><span className={style.title}>SCHEDULE:</span> {showDetails.schedule.time ? showDetails.schedule.time : "No time"} -
+          {showDetails.schedule.days === [] ? showDetails.schedule.days.map((day, index) => <span key={index}>{day}</span>) : " No day"}</p>
+        <p><span className={style.title}>SUMMARY:</span> {showDetails.summary ? removeTags(showDetails.summary) : "No summary"}</p>
 
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default ShowDetails;
