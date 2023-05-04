a3269d05105472abb4fe66d0bbd0b74bd3bf1629
ErastKoliasa <erastyk28@gmail.com>
2023-05-04 21:36:58 +0300

    added styles for search field

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,6 +1,8 @@
+import style from './SearchField.module.css'
+
 const SearchField = ({handleSearch}) => {
     return(
-        <div>
+        <div className={style.containerSearchField}>
             <input type="text" placeholder="Search TV show" onChange={handleSearch}/>
         </div>
     )
