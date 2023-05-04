ee232b7fed2715eb21cb1197b4fb993f4d86ee93
ErastKoliasa <erastyk28@gmail.com>
2023-05-04 21:37:35 +0300

    added styles for shows card

--- src/components/ShowsList/ShowsList.jsx
+++ src/components/ShowsList/ShowsList.jsx
@@ -1,17 +1,20 @@
-import photo from "../../img/missing.jpg" 
-const ShowsList = ({shows, handleClick}) => {
-    return(
+import photo from "../../img/missing.jpg"
+import style from './ShowsList.module.css'
+const ShowsList = ({ shows, handleClick }) => {
+    return (
         <div>
-            { shows.length === 0 ? "Sorry, nothing found with this search" :
-                shows.map((item) => {
-                    return (
-                        <div key={item.show.id} onClick={e => handleClick(item.show.id)}>
-                            {item.show.image ? <img src={item.show.image.medium}  alt="Movie banner" /> : <img src={photo} alt="Movie banner is missing" />}
-                            <h2>{item.show.name}</h2>
-                            <h3>RATING: {item.show.rating.average ? item.show.rating.average : "No rating"}</h3>
-                        </div>
-                    )
-                })
+            {shows.length === 0 ? <p className={style.typeName}>Sorry, nothing found with this search</p> :
+                <div className={style.cardContainer}>{
+                    shows.map((item) => {
+                        return (
+                            <div className={style.showCard} key={item.show.id} onClick={e => handleClick(item.show.id)}>
+                                {item.show.image ? <img src={item.show.image.medium} alt="Movie banner" /> : <img className={style} src={photo} alt="Movie banner is missing" />}
+                                <h2>{item.show.name}</h2>
+                                <h3>{item.show.rating.average ? item.show.rating.average : "No rating"}</h3>
+                            </div>
+                        )
+                    })}
+                </div>
             }
         </div>
     )
