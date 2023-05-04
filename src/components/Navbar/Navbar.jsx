a99ec6fad767b87d731037937382fd4b6f14def3
ErastKoliasa <erastyk28@gmail.com>
2023-05-04 21:35:42 +0300

    added styles for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-
+import style from './Navbar.module.css'
 const Navbar = () => {
     return(
-        <nav>
-            <Link to={"/"}>HOME</Link>
+        <nav className={style}>
+            <Link className={style.link} to={"/"}>HOME</Link>
         </nav>
     )
 }
